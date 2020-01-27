04f6cf60c5801a87d6518552db6e964351b32d45
sasha walker <sashawalker31@gmail.com>
2020-01-27 01:53:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,18 @@
 
 
-def countdown(number)
-  number =10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
- puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  def countdown(number)
-  while number > 0
-    puts "#{number} SECONDS(S)!"
-    sleep 1
-    number -= 1
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECONDS(S)!"
+    sleep (1)
+    seconds_to_midnight -= 1
   end
- puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
